e41987064ad8296567cb9054ecb45f5040f39109
Guillaume Astorgis <guillaume.astorgis@epitech.eu>
2019-03-13 18:19:47 +0100

    Menus
    - Added Directory Menu object class
    - Added menu entry subobject struct

---
+++ core_build/include/DirectoryMenu.hpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2019
+** Arcade
+** File description:
+** directory menu object class
+*/
+
+#ifndef DIRECTORYMENU_HPP_
+    #define DIRECTORYMENU_HPP_
+
+	#include <string>
+	#include <vector>
+
+class DirectoryMenu {
+	public:
+
+		struct MenuEntry {
+			public:
+				MenuEntry(const std::string &path,
+						  const std::string &prefix = "",
+						  const std::string &suffix = "");
+				~MenuEntry() = default;
+
+				std::string			name;
+				const std::string	path;
+		};
+
+		typedef std::vector<MenuEntry>	Menu;
+
+		DirectoryMenu(const std::string &dir,
+					  const std::string &prefix = "",
+					  const std::string &suffix = "");
+		~DirectoryMenu() = default;
+
+		void			refresh();
+		Menu::iterator	begin();
+		Menu::iterator	end();
+
+		const MenuEntry	&operator[](std::size_t idx) const;
+
+	protected:
+	private:
+
+		std::string		_dir;
+		std::string		_prefix;
+		std::string		_suffix;
+		Menu			_entries;
+		Menu::iterator	_cursor;
+};
+
+#endif /* !DIRECTORYMENU_HPP_ */
